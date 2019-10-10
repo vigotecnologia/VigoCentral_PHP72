4fe9464f101ae0f1e9eb9fdfd1b01bb602f5ab15
Rafael Labiak Olivastro <rolivastro@hotmail.com>
2019-10-09 22:56:01 -0400

    Ajustes.

--- models/mksenha_model.php
+++ models/mksenha_model.php
@@ -15,13 +15,13 @@ class MkSenha_Model extends Model {
     public function Troca_MkSenha($id_cliente, $mk_login, $mk_senha) {
 
         $this->Conecta("mikrotik");
+
         $query = "UPDATE radcheck SET Value='" . $mk_senha . "' WHERE id_cliente='" . $id_cliente . "' AND Username='" . $mk_login . "' AND Attribute='ClearText-Password'";
         $row = $this->read($query);
-        $this->Desconecta();
 
-        $this->Conecta("mikrotik");
         $query = "UPDATE radcheck SET Value=md5('" . $mk_senha . "') WHERE id_cliente='" . $id_cliente . "' AND Username='" . $mk_login . "' AND Attribute='MD5-Password'";
         $row = $this->read($query);
+
         $this->Desconecta();
 
         return $row;
--- models/servicos_model.php
+++ models/servicos_model.php
@@ -5,7 +5,7 @@ class Servicos_Model extends Model {
     public function Lista_Servicos($cliente) {
 
         $this->Conecta();
-        $query = "SELECT * FROM financeiro_planos_clientes WHERE idcliente='" . $cliente . "'";
+        $query = "SELECT * FROM financeiro_planos_clientes WHERE idcliente='" . $cliente . "' ORDER BY descricao";
         $row = $this->read($query);
         $this->Desconecta();
 
