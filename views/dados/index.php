9f957836fb626b64e6a7a122f58cda9a137d502b
Rafael Labiak Olivastro <rolivastro@hotmail.com>
2019-10-18 22:22:13 -0400

    Limpeza.

--- views/dados/index.php
+++ views/dados/index.php
@@ -3,20 +3,16 @@ require_once 'libs/Functions.php';
 $funcoes = new Functions(); // Instancia a classe de FUNÇÕES BÁSICAS
 $funcoes->verificaSessao();
 ?>
-<!-- SECAO: DADOS -->
-<section class="dados">			
+<section class="dados">
     <div class="container">
         <h1>Meus Dados</h1>
-
         <ul class="caminho">
             <span>Voc&ecirc; est&aacute; em: </span>
             <li class="target"><a href="core">HOME</a></li>
             <li class="target"><a href="conta">MINHA CONTA</a></li>
             <li class="target">MEUS DADOS</li>
         </ul>
-
         <h3>Dados Pessoais</h3>
-
         <label>
             <span>Nome:</span>
             <strong class="maiusculo"><?php echo utf8_decode(utf8_encode($this->cliente->nome)); ?></strong>
@@ -33,9 +29,7 @@ $funcoes->verificaSessao();
             <span>Data de Nascimento:</span>
             <strong><?php echo $funcoes->dataToBR($this->cliente->dt_nascimento); ?></strong>
         </label>
-
         <h3>Endere&ccedil;o e Contato</h3>
-
         <label>
             <span>Endere&ccedil;o:</span>
             <strong class="maiusculo"><?php echo utf8_decode(utf8_encode($this->cliente->endereco . ' - ' . $this->cliente->bairro)); ?></strong>
@@ -60,9 +54,7 @@ $funcoes->verificaSessao();
             <span>E-mail:</span>
             <strong><?php echo utf8_decode(utf8_encode($this->cliente->email)); ?></strong>
         </label>
-
         <h3>Dados da Conta</h3>
-
         <label>
             <span>Dia de Vencimento:</span>
             <strong><?php echo utf8_decode(utf8_encode($this->cliente->vencimento)); ?></strong>
@@ -71,7 +63,6 @@ $funcoes->verificaSessao();
             <span>Cliente Desde:</span>
             <strong><?php echo $funcoes->dataToBR($this->cliente->dt_entrada); ?></strong>
         </label>
-
         <div class="clear"></div>
     </div><div class="clear"></div>
-</section><!-- /FIM - SECAO: DADOS -->
+</section>
